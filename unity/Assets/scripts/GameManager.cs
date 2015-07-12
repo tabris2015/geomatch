9d0cebf78eb92943059b5d8889139889db1a48c3
tabris2015 <eduardo.laruta@gmail.com>
2015-07-12 09:02:45 -0400

    finalizados scripts

--- unity/Assets/scripts/GameManager.cs
+++ unity/Assets/scripts/GameManager.cs
@@ -5,10 +5,12 @@ public class GameManager : MonoBehaviour {
 
 
 	//variables de estado del juego
+
 	public GameObject GAME;
 	public GameObject MENU;
+	public UITweener menu_final;
 
-
+	public UILabel descripcion;
 	public UILabel nivel_label;
 	public UILabel score_label;
 
@@ -16,8 +18,12 @@ public class GameManager : MonoBehaviour {
 	public int nivel_actual;
 	public GameObject[] niveles;
 	public GameObject[] fichas;
+	public AudioClip[] canciones_niveles;
+	public AudioClip cancion_menu;
 
-	private int niveles_max = 3;
+	bool menu = true;
+
+	private int niveles_max = 3;	
 
 	private int cont_aciertos = 0;
 
@@ -27,6 +33,11 @@ public class GameManager : MonoBehaviour {
 
 		MENU.SetActive(true);
 		GAME.SetActive(false);
+
+		//cancion_menu.Play();
+
+		audio.clip = cancion_menu;
+		audio.Play();
 
 		NotificationCenter.DefaultCenter().AddObserver(this, "match");
 		//NotificationCenter.DefaultCenter().AddObserver(this, "level_clear");
@@ -49,6 +60,9 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(nivel_actual == 0) descripcion.text = "ubica las banderas!";
+		if(nivel_actual == 1) descripcion.text = "ubica los monumentos!";
+		if(nivel_actual == 2) descripcion.text = "ubica las personas!";
 
 	}
 
@@ -61,16 +75,24 @@ public class GameManager : MonoBehaviour {
 		Debug.Log("desde noti!");
 		if(cont_aciertos >= 6)
 		{
+			audio.Stop();
+			audio.clip = canciones_niveles[nivel_actual];
+			audio.Play();
+
+
 			NotificationCenter.DefaultCenter().PostNotification(this, "level_clear", nivel_actual);
 			cont_aciertos = 0;
 			nivel_actual++;
+
 			nivel_label.text = nivel_actual.ToString();
 
 			Debug.Log("siguiente nivel");
-			if(nivel_actual > niveles_max)
+			if(nivel_actual >= niveles_max)
 			{
 				NotificationCenter.DefaultCenter().PostNotification(this, "victory");
 				Debug.Log("ganaste");
+				//GAME.SetActive(false);
+				menu_final.Toggle();
 			}
 			else 
 			{
@@ -101,5 +123,9 @@ public class GameManager : MonoBehaviour {
 		MENU.SetActive(false);
 		GAME.SetActive(true);
 
+	}
+	public void final()
+	{
+		Application.LoadLevel(2);
 	}
 }
